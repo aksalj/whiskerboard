be3697360eb2e5aff0e5b7be01047974589f94fc
Salama A.B <aksalj@aksalj.me>
2014-06-21 21:08:28 +0300

    Prep for live runs

--- settings/base.py
+++ settings/base.py
@@ -1,4 +1,6 @@
 from unipath import FSPath as Path
+import os
+
 PROJECT_DIR = Path(__file__).absolute().ancestor(2)
 
 ######################################
@@ -40,11 +42,11 @@ MIDDLEWARE_CLASSES = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'data/whiskerboard.db',
-        'USER': 'whiskerboard',
-        'PASSWORD': 'whiskerboard',
-        'HOST': '',
-        'PORT': '',
+        'NAME': os.environ['WB_DB_NAME'],
+        'USER': os.environ['WB_DB_USER'],
+        'PASSWORD': os.environ['WB_DB_PASSWORD'],
+        'HOST': os.environ['WB_DB_HOST'],
+        'PORT': os.environ['WB_DB_PORT'],
     }
 }
 
--- settings/live.py
+++ settings/live.py
@@ -1,17 +1,8 @@
 from __future__ import absolute_import
-from .base import *
-from .local import *
 
-#CACHE_BACKEND = 'redis_cache.cache://127.0.0.1:6379/?timeout=15'
+# Your live settings go here?
+
 DEBUG = False
 
-# Openshift Settings
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': '$OPENSHIFT_DATA/database.db',
-    'USER': 'whiskerboard',
-    'PASSWORD': 'mHxXFxyTd5N29W9qGYHMbQL6',
-    'HOST': '',
-    'PORT': '',
-}
+#CACHE_BACKEND = 'redis_cache.cache://127.0.0.1:6379/?timeout=15'
 
